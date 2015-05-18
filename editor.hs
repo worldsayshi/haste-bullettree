b101114d8c0d1e338f733b150004c2e5921b5bc8
Per Fredelius <per.fredelius@findwise.com>
2015-05-19 00:06:44 +0200

    WIP adding and removing nodes

--- editor.hs
+++ editor.hs
@@ -53,14 +53,7 @@ renderTree store allTree tree ixs = do
         ! atr "value" (tree ^. _text)
         ! atr "data-index" (show ixs)
         `addEvent`
-          Change $ \ev -> do
-          els <- elemsByQS document ("input[data-index=\""++show ixs++"\"]")
-          forM_ els (
-            \el -> do
-              val <- getProp el "value"
-              let newTree = allTree & _textAt ixs .~ val
-              writeLog ("result: "++val)
-              onServer $ (setTree store) <.> (newTree)))
+          Change $ \ev -> updateWithInputValue allTree ixs store)
         `addEvent` KeyDown $ \keycode -> do
            case keycode of
              9 -> (liftIO preventDefault) >> writeLog "Tab!"
@@ -70,18 +63,34 @@ renderTree store allTree tree ixs = do
             
         `addEvent` KeyUp $ \keycode -> do
           case keycode of
-            13 -> writeLog "Enter!"
+            13 -> (liftIO preventDefault) >> insertNodeAfter allTree ixs store
             38 -> (liftIO preventDefault) >> writeLog "Up arrow!"
             40 -> (liftIO preventDefault) >> writeLog "Down arrow!"
+            -- Todo check also if it has child trees. Then abort.
+            8 -> if allOf (_textAt ixs) (=="") allTree
+                 then (liftIO preventDefault) >> removeNodeAt allTree ixs store
+                 else return ()
             _  -> writeLog $ "KeyUp: " ++ show keycode)
     forM_ (zip [0..] (tree ^. _subtrees)) $ \(ix,subtree) -> do
       renderTree store allTree subtree (ixs++[ix])
+
+
+{-- ACTIONS --}
 
 --navTo :: (MonadIO m, Show a, Functor m) => [a] -> m ()
 navTo ixs = do
   -- TODO sometimes getting pattern match error here, can't reproduce...
   elem:[] <- elemsByQS documentBody $ "input[data-index=\""++show ixs++"\"]"
   focus elem
+
+updateWithInputValue tree ixs store = do
+  els <- elemsByQS document ("input[data-index=\""++show ixs++"\"]")
+  forM_ els (
+    \el -> do
+      val <- getProp el "value"
+      let newTree = tree & _textAt ixs .~ val
+      writeLog ("result: "++val)
+      onServer $ (setTree store) <.> (newTree))
 
 --moveCursorUp :: (MonadIO m, Functor m) => [Int] -> m ()
 moveCursorUp tree ixs =
@@ -104,4 +113,29 @@ moveCursorDown tree ixs = do
         then navTo ((init parentIxs) ++ [(last parentIxs) + 1])
         else navTo (init ixs ++ [n+1])
         
-  
+insertNodeAfter tree ixs store = do
+  let parentIxs = init ixs
+      fringeIx = last ixs
+      newTree = tree & _treeAt parentIxs %~
+                (\parentTree ->
+                  parentTree & _subtrees %~
+                  (\trees ->
+                    let nextIx = fringeIx + 1
+                    in take nextIx trees ++ [Tree "" []] ++ drop nextIx trees))
+      newIxs = parentIxs ++ [fringeIx + 1]
+  onServer $ (setTree store) <.> (newTree)
+  -- This navTo should be scheduled to react to a rerended event?
+  -- Timing problems can occur
+  navTo newIxs
+
+removeNodeAt tree ixs store = do
+  let parentIxs = init ixs
+      fringeIx = last ixs
+      newTree = tree & _treeAt parentIxs %~
+                (\parentTree ->
+                  parentTree & _subtrees %~
+                  (\trees ->
+                    take fringeIx trees ++ drop (fringeIx+1) trees))
+  onServer $ (setTree store) <.> (newTree)
+  -- Todo check if sibling above
+  navTo parentIxs
